4a15ed27693b0a67a1c0ba43b2cbc7821d259abc
Nick <nicholas.zarate@gmail.com>
2016-10-29 15:13:36 -0600

    something

--- web/07/home.php
+++ web/07/home.php
@@ -11,16 +11,16 @@
   <script type="text/javascript">
     function refreshEvents() {
       console.log("refresh events called");
-      $.get('http://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
-        $(data).find("item").each(function () { // or "item" or whatever suits your feed
-          var el = $(this);
-          console.log("------------------------");
-          console.log("title      : " + el.find("title").text());
-          console.log("author     : " + el.find("author").text());
-          console.log("description: " + el.find("description").text());
-        });
-      });
-    }
+    //   $.get('http://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
+    //     $(data).find("item").each(function () { // or "item" or whatever suits your feed
+    //       var el = $(this);
+    //       console.log("------------------------");
+    //       console.log("title      : " + el.find("title").text());
+    //       console.log("author     : " + el.find("author").text());
+    //       console.log("description: " + el.find("description").text());
+    //     });
+    //   });
+    // }
   </script>
 </head>
 <body>
