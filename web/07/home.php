dd2c7bd0409c097f8181f16fe9d335f256943451
Nick <nicholas.zarate@gmail.com>
2016-10-29 14:02:55 -0600

    call correct other functions file

--- web/07/home.php
+++ web/07/home.php
@@ -12,7 +12,7 @@ require('db_connection.php');
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="./otherFunctions.js"></script>
+  <script src="./07/otherFunctions.js"></script>
 </head>
 <body>
 
--- web/07/otherFunctions.js
+++ web/07/otherFunctions.js
@@ -1,4 +1,5 @@
 function refreshEvents() {
+  console.log("refresh events called");
   $.get('http://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
     $(data).find("item").each(function () { // or "item" or whatever suits your feed
       var el = $(this);
