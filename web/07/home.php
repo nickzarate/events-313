d0096ba36d2050dc68ff02c48357e7fad7d0256f
Nick <nicholas.zarate@gmail.com>
2016-10-29 10:54:41 -0600

    Display all events on events page

--- web/07/home.php
+++ web/07/home.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+
+<?php
+require('db_connection.php');
+?>
+
 <html lang="en">
 <head>
   <title>Bootstrap Case</title>
@@ -11,14 +16,26 @@
 <body>
 
 <div class="container">
-  <h2>CS 313</h2>
+  <h2>Welcome <?= $row['username']; ?>!</h2>
   <ul class="nav nav-tabs">
     <li class="active"><a data-toggle="tab" href="#all_events">All Events</a></li>
     <li><a data-toggle="tab" href="#my_events">My Events</a></li>
   </ul>
   <div class="tab-content">
     <div id="all_events" class="tab-pane fade in active">
-      <h3>Welcome <?= $row["username"]; ?></h3>
+      <?php
+        foreach ($db->query('SELECT * FROM event') -> fetchAll() as $events) {
+          // if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['form'] == 'form2') {
+          //   if ($_POST["books"] == $books["book"]) {
+          //     $selected = "selected='selected'";
+          //   } else {
+          //     $selected = "";
+          //   }
+          // }
+          echo '<h4>' . $events['title'] . '</h4>' . '<p>' . $events['description'] . '</p>';
+        }
+      ?>
+      <p>Here will be all events in the database</p>
     </div>
     <div id="my_events" class="tab-pane fade">
       <h3>My Events</h3>
