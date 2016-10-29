f66928f28c3bdd23823ae67cd9c990c85d1684a1
Nick <nicholas.zarate@gmail.com>
2016-10-29 14:36:50 -0600

    dsf

--- web/07/functions.php
+++ web/07/functions.php
@@ -81,17 +81,4 @@
     $row = $result->fetch();
     return $row;
   }
-  function refreshEvents() {
-    console.log("refresh events called");
-    $.get('http://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
-      $(data).find("item").each(function () { // or "item" or whatever suits your feed
-        var el = $(this);
-        console.log("------------------------");
-        console.log("title      : " + el.find("title").text());
-        console.log("author     : " + el.find("author").text());
-        console.log("description: " + el.find("description").text());
-      });
-    });
-  }
-
 ?>
