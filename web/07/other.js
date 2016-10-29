6b31458ba572027173de0703cb1056f080321648
Nick <nicholas.zarate@gmail.com>
2016-10-29 15:19:43 -0600

    secure

--- web/07/other.js
+++ web/07/other.js
@@ -1,6 +1,6 @@
 function refreshEvents() {
   console.log("refresh events called");
-  $.get('http://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
+  $.get('https://calendar.byui.edu/RSSFeeds.aspx?data=tq9cbc8b%2btuQeZGvCTEMSP%2bfv3SYIrjQ3VTAXA335bE0WtJCqYU4mp9MMtuSlz6MRZ4LbMUU%2fO4%3d', function (data) {
     $(data).find("item").each(function () { // or "item" or whatever suits your feed
       var el = $(this);
       console.log("------------------------");
