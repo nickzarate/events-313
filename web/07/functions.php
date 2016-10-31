22259c5bbb8d5be35830bc88e22c0c32fe660051
Nick <nicholas.zarate@gmail.com>
2016-10-30 22:03:09 -0600

    Fix

--- web/07/functions.php
+++ web/07/functions.php
@@ -82,14 +82,12 @@
     return $row;
   }
   function getFeed($feed_url) {
-
     $content = file_get_contents($feed_url);
     $x = new SimpleXmlElement($content);
 
     echo "<ul>";
-
     foreach($x->channel->item as $entry) {
-        echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";
+        echo "<li>" . $entry->title . "</li>";
     }
     echo "</ul>";
 }
